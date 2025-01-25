b66932c2b12e8c62ff00f7c8dd6625a54249fe32
jonej <jonejabari123@gmail.com>
2025-01-25 12:53:43 +0200

    Merge remote-tracking branch 'origin/update' into update

--- src/main/java/io/jone/secureapi/query/UserQuery.java
+++ src/main/java/io/jone/secureapi/query/UserQuery.java
@@ -1,7 +1,7 @@
 package io.jone.secureapi.query;
 
 public class UserQuery {
-    public static final String INSERT_USER_QUERY = "INSERT INTO users (first_name, last_name, email, password) VALUES (:firstName, :lastName, :email, :password";
-    public static final String COUNT_USER_EMAIL_QUERY = "SELECT COUNT(*) FROM users WHERE email = :email";
-    public static final String INSERT_ACCOUNT_VERIFICATION_URL_QUERY = "INSERT INTO account_verifications (user_id, url) VALUES (:userId, url";
+    public static final String INSERT_USER_QUERY = "INSERT INTO Users (first_name, last_name, email, password) VALUES (:firstName, :lastName, :email, :password)";
+    public static final String COUNT_USER_EMAIL_QUERY = "SELECT COUNT(*) FROM Users WHERE email = :email";
+    public static final String INSERT_ACCOUNT_VERIFICATION_URL_QUERY = "INSERT INTO AccountVerifications (user_id, url) VALUES (:userId, url)";
 }
